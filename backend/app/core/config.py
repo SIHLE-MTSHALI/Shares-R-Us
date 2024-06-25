21134d285e2c0abd0c37688857189b001bddac89
Sihle Mtshali <sihle.mtshali8@gmail.com>
2024-06-26 00:05:16 +0200

    Fixing a few issue on the Database setup

--- backend/app/core/config.py
+++ backend/app/core/config.py
@@ -2,9 +2,12 @@ import os
 from pydantic_settings import BaseSettings
 
 class Settings(BaseSettings):
-    DATABASE_URL: str = os.getenv("DATABASE_URL", "postgres://uegl9oa86r9mn2:pc25bfa51b23e639e6a3553e2e54568b056bd95f811628d1f50bb989bc785d764@c6sfjnr30ch74e.cluster-czrs8kj4isg7.us-east-1.rds.amazonaws.com:5432/d7igsbgc5vvr1l")
-    SECRET_KEY: str = "your-secret-key"
+    DATABASE_URL: str = os.getenv("DATABASE_URL")
+    SECRET_KEY: str = os.getenv("SECRET_KEY")
     ALGORITHM: str = "HS256"
     ACCESS_TOKEN_EXPIRE_MINUTES: int = 30
+
+    class Config:
+        env_file = ".env"
 
 settings = Settings()
